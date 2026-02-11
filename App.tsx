f8f4c1dd50f984f07b800b2aea7f247bad1a0000
harrypickup <harrypickup1@hotmail.com>
2026-02-11 17:29:58 +0000

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,43 @@
+
+import React, { useEffect } from 'react';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Challenges from './pages/Challenges';
+import Infrastructure from './pages/Infrastructure';
+import Performance from './pages/Performance';
+import HowItWorks from './pages/HowItWorks';
+import Contact from './pages/Contact';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/challenges" element={<Challenges />} />
+            <Route path="/infrastructure" element={<Infrastructure />} />
+            <Route path="/performance" element={<Performance />} />
+            <Route path="/how-it-works" element={<HowItWorks />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
